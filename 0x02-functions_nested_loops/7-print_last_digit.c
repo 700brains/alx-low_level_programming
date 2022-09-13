de7b71f4c14336c574fa934841ae5471b90e620d
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-13 16:54:05 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of integer
+ * @n: integer argument
+ * Return: last digit of number
+ */
+
+int print_last_digit(int num)
+{
+	int n = mun % 10;
+	if (n < 0)
+		n = -n;
+
+       	_putchar(n + '0');
+
+	return (n);
+}
