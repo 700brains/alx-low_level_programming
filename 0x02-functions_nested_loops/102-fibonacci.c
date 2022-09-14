d706a8691f48610fd3b7ffc1206d9c8b32983ef7
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-13 17:37:18 -0700

    Task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,14 +15,15 @@ int main(void)
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
 		printf("%li %li ", a, b);
+		printf(',');
 		a += b;
 		b += a;
 	}
 	if (countto % 2 == 1)
-	{
+	
 		printf("%li", a);
-		printf(',');
-	}
+	
+	
 
 	printf("\n");
 
