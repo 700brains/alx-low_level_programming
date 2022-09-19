0c5d210c72747696b2da1e4e054f1d1da20773b9
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-19 13:56:01 -0700

    Task password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,43 +9,24 @@
  */
 
 int password(int password) {
-char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!QWERTYUIOPASDFGHJKLZXCVBNM";
+char list[] = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 printf("\t");
-int i;
+int i,j,k;
 for(i = 0; i < password; i++) {
     printf("*");
 }
 printf("\n");
 printf("\t");
 srand(time(NULL));
-for(i = 0; i < password; i++) {
+
+for(j = 0; j < password; j++) {
     printf("%c", list[rand() % (sizeof list - 1)]);
 }
 printf("\n");
 printf("\t");
-for(i = 0; i < password; i++) {
+
+for(k = 0; k < password; k++) {
     printf("*");
 }
 printf("\n");
-}
-
-/**
- * main - writes passwords
- * @void: Empty parameter list for main.
- *
- * Description: Writes ask for length and generate a random number
- *
- * Return: 0 for success
-*/
-int main(void) {
-int password_length;
-printf("\n\t*********************************\n\n");
-printf("\tWelcome to the password generator\n\n");
-printf("\t*********************************\n");
-printf("\n\tEnter length of the password = ");
-scanf("%d", &password_length);
-printf("\n");
-printf("\n");
-password(password_length);
-return 0;
 }
