22d9ef752d23a6d3f25a8e9d9fd8d9a47eac4548
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-19 17:48:14 -0700

    Task password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,35 +1,53 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /**
- * password - generates a random password
- * 
- * Return: void
+ * main - Generates random valid password for the
+ * program 101-crackme.
+ * Return: Always 0.
  */
-
-void password(void)
-{
-
-
-	srand(time(NULL));
-	
-	printf("Tada! Congrats");
-
-}
-
-/**
- * main - writes passwords
- * @void: Empty parameter list for main.
- *
- * Description: Writes ask for length and generate a random number
- *
- * Return: 0 for success
-*/
 int main(void)
 {
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	password();
-	
-	return 0;
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
+	return (0);
 }
