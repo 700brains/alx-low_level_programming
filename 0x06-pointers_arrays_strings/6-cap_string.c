6f890cf629d3770eeff6a16c9e7c2c92dd4975b7
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-21 12:09:37 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,6 +7,35 @@
  * Return: A pointer to the changed string.
  */
 
+
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	/* check first index for capital */
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
+	while (s[i] != '\0') /* iterate through string */
+	{
+
+		/* if lowercase and prior char is separator, capitalize*/
+		if ((s[i] >= 'a' && s[i] <= 'z')
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+
+	return (s);
+}
+
+/**
 char *cap_string(char *str)
 {
 	int index = 0;
@@ -36,4 +65,4 @@ char *cap_string(char *str)
 	}
 
 	return (str);
-}
+}*/
