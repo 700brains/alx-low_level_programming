2908ad10e0e4510506d394350d02ec90d8cca179
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-21 12:24:10 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,13 @@
  * of the first unmatched characters.
  */
 
-
 int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		while (*s1 && *s2 && *s1 == *s2)
-			{
-				s1++;
-				s2++;
-			}
-		return (*s1 - *s2);
-
+		s1++;
+		s2++;
 	}
+
+	return (*s1 - *s2);
+}
