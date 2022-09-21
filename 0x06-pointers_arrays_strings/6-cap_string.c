f83632d9295cc79ba2bfbdb8904f112dec489c06
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-21 12:17:03 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,13 +1,10 @@
-#include "main.h"
+#include "main.h";
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
+ * cap_string - capitalize first letter of each word
+ * @s: string to manipulate
+ * Return: string
  */
-
-
 
 char *cap_string(char *s)
 {
@@ -34,35 +31,3 @@ char *cap_string(char *s)
 
 	return (s);
 }
-
-/**
-char *cap_string(char *str)
-{
-	int index = 0;
-
-	while (str[index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
-		index++;
-	}
-
-	return (str);
-}*/
