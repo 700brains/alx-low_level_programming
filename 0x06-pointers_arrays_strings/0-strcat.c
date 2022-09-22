a6476728af6c6b48fd63ec7094adb5b1d9eccc86
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-22 01:08:25 -0700

    Task Zero

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,22 +2,20 @@
 
 
 /**
- *_strcat - concatenate two strings
- * @dest: first string
- * @src: second string
- * Return: dest
- */
+ * _strcat - concatenates two strings
+ * @src: second string to copy from
+ * @dest: string to be overwritten
+ * Description: concatenates src to end of dest
+ * Return: pointer to dest
+ **/
 
 char *_strcat(char *dest, char *src)
-{
-	int a = 0, j = 0;
+	{
+		int a = 0, j = 0;
 
-	while (dest[a++])
-		j++;
-	
-	for(a = 0; src[a]; a++)
-		dest[j++] = src[a];
-	
-
-	return (dest);
-}
+		while (dest[a++])
+			j++;
+		for (a = 0; src[a]; a++)
+			dest[j++] = src[a];
+		return (dest);
+	}
