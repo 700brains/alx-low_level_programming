4011f3d602c4a0e24787eddb4378e882db58f182
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-28 11:27:54 -0700

    Task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,18 +11,19 @@
 int wildcmp(char *s1, char *s2)
 {
 	int i = 0, j = 0;
+
 	while (s1[i] != '\0' && s2[j] != '\0')
-	{
-		if (s1[i] == s2[j])
-			i++; j++;
-		if (s2[j] == "*")
 		{
-			j++;
-			while ((s1[i] != s2[j]) && s1[i] != '\0')
-				i++;
+			if (s1[i] == s2[j])
+				i++; j++;
+			if (s2[j] == "*")
+				{
+					while ((s1[i] != s2[j]) && s1[i] != '\0')
+						i++;
+				
+				}
+			if (s1[i] != s2[j])
+				return (0);
 		}
-		if (s1[i] != s2[j])
-			return (0);
-	}
 	return (1);
 }
