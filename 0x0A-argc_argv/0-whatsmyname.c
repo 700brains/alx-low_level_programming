b43d68a6022d5f836011475e466171784d3a39f4
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-09-30 06:15:06 -0700

    Added main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints program name, followed bt a new line
+ * @argc: int
+ * @argv: list
+ * Return 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+
+	_putchar("\n");
+
+	return (0);
+
+}
